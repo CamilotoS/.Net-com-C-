dff8fed09fa73654c6cc0fdad1f68890d5f83cd3
Camiloto_ <ivescamiloto1@gmail.com>
2021-01-30 15:27:45 -0300

    Atualização File

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 ### Estudo e Projetos com .Net em C#
 
 ###### Conteudos desta pasta
+
+Ambiente de Desenvolvimento 
+- Prepara
+
+Conhecendo o C#
+    O que é o C#
+    Como Funciona
+    Estrutura de Programa
+    Tipos e Variaveis
+        Tipos de Valor
+        Tipo de Referencia
+    Intruções :
+        if
+        switch
+        while
+        do
+        for
+        break
+        continue
+        return
+        throw
+        try..catch..finally
+        using
+        foreach
+    Arrays
+    Classes e Objetos
+        Membros
+        Acessibilidade
+        Herança
+        Métodos
+    Structs
+    Interfaces
+    Enums
+
---
+++ Sistema de Aluno e Notas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Revisao
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            Console.WriteLine("Bem Vindo ao Sistema de Notas !");
+            do
+            {
+                
+                Console.WriteLine("Escolha uma das opções abaixo para continuar :");
+                opc();
+                opt = Convert.ToInto32(Console.WriteLine());
+                Console.WriteLine($"A opção Escolhida foi {opt}");
+
+                switch (opt)
+                {
+                    case 1:
+                        Console.WriteLine($"Escolheu a Opção de numero {opt}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Escolheu a Opção de numero {opt}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Escolheu a Opção de numero {opt}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Escolheu a Opção de numero {opt}");
+                        break;
+                    default:
+                        //throw new ArgumentNullException();
+                        break;
+
+                }   
+            }while(opt != 5);
+        }
+
+        private static void opc()
+        {
+            Console.WriteLine("1 - Inserir novo Aluno");
+            Console.WriteLine("2 - Inserir Notas para Aluno");
+            Console.WriteLine("3 - Deletar Aluno Cadastrado");
+            Console.WriteLine("4 - Deletar Notas Colocada");
+            Console.WriteLine("5 - SAIR");
+        }
+    }
+}
